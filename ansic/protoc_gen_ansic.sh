5e2bc68e88f4d76803301f874b1636a9575da3bc
chuckpeplinski <chuck.peplinski@i3pd.com>
2023-09-11 12:10:57 -0500

    Deleted services.proto.  Moved the header into reach.proto.

--- ansic/protoc_gen_ansic.sh
+++ ansic/protoc_gen_ansic.sh
@@ -1,3 +1,3 @@
 rm -rf generated
 mkdir generated
-./nanopb/generator/nanopb_generator.py -I../proto --output-dir=./generated services.proto reach.proto
+./nanopb/generator/nanopb_generator.py -I../proto --output-dir=./generated reach.proto
