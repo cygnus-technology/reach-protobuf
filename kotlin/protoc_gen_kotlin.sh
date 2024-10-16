43930d2b4918557f96826ab5d7bbff34a86b119c
Conner Christianson <conner.christianson@cygnustechnology.com>
2024-10-16 09:46:17 -0500

    Edit kotlin generation script

--- kotlin/protoc_gen_kotlin.sh
+++ kotlin/protoc_gen_kotlin.sh
@@ -1,2 +1,2 @@
-rm -rf ../../reachprotocol/src/main/java/com/cygnusreach/reachprotocol/proto
-protoc -I=../proto --kotlin_out=../../reachprotocol/src/main/java --java_out=../../reachprotocol/src/main/java reach.proto
+rm -rf ../../reachprotocol/src/main/proto
+protoc -I=../proto --kotlin_out=../../java --java_out=../../java reach.proto
